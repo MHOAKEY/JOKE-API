e03205fbf9e840e2bdf392485bc27cef9a06c584
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-31 15:44:48 -0400

    added button for two part joke. When clicked joke container inner HTML will include setup and delivery.

--- script.js
+++ script.js
@@ -9,8 +9,20 @@ function randomJoke() {
     });
 }
 
+function twoPartJoke() {
+  fetch("https://v2.jokeapi.dev/joke/Any?type=twopart")
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      renderJoke(jsonResponse);
+    });
+}
+
 function renderJoke(joke) {
-  console.log(joke.joke);
-  let jokeString = `<p>${joke.joke}</p>`;
+  if (joke.type === "single") {
+    let jokeString = `<p>${joke.joke}</p>`;
+    jokeCont.innerHTML = jokeString;
+  } else jokeString = `<p>${joke.setup}</p>`;
+  jokeString += `<p>...</p>`;
+  jokeString += `<p>${joke.delivery}</p>`;
   jokeCont.innerHTML = jokeString;
 }
